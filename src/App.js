346562cb78d7f923573673592c88837cb3b487b9
Keshav Kishor Ram <ramabishek40@gmail.com>
2021-10-25 18:51:40 +0545

    Created Navbar basic layout

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
