d27a19bc6cbecce652859664ad881fcddb36427e
Keshav Kishor Ram <ramabishek40@gmail.com>
2021-10-29 19:58:13 +0545

    Feat: Use Formik in Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,9 +2,20 @@ import React from "react";
 import "styles/register.scss";
 import clsx from "clsx";
 import { useSelector } from "react-redux";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 export default function Login() {
   const { dark } = useSelector((state) => state.common);
+  const loginSchema = Yup.object({
+    email: Yup.string().email("Invalid email").required("Required"),
+    password: Yup.string().required("Required"),
+  });
+
+  const initialValues = { email: "", password: "" };
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
   return (
     <div
       className={clsx(
@@ -13,35 +24,50 @@ export default function Login() {
       )}
     >
       <div className="container">
-        <form className="card p-4">
-          <h2 className="text-center">Login</h2>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={loginSchema}
+          onSubmit={handleSubmit}
+        >
+          {(errors, touched) => (
+            <Form className="card p-4">
+              <h2 className="text-center">Login</h2>
 
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email <span className="text-danger">*</span>
-            </label>
-            <input
-              type="email"
-              className="form-control shadow-none"
-              required={true}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              Password <span className="text-danger">*</span>
-            </label>
-            <input
-              type="password"
-              name="password"
-              className="form-control shadow-none"
-              required={true}
-            />
-          </div>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email <span className="text-danger">*</span>
+                </label>
+                <Field
+                  name="email"
+                  type="email"
+                  className="form-control shadow-none"
+                  required={true}
+                />
+                <small className="error-message text-danger">
+                  <ErrorMessage name="email" />
+                </small>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Password <span className="text-danger">*</span>
+                </label>
+                <Field
+                  type="password"
+                  name="password"
+                  className="form-control shadow-none"
+                  required={true}
+                />
+                <small className="error-message text-danger">
+                  <ErrorMessage name="password" />
+                </small>
+              </div>
 
-          <button type="submit" className="submit-btn btn btn-primary">
-            Login
-          </button>
-        </form>
+              <button type="submit" className="submit-btn btn btn-primary">
+                Login
+              </button>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
