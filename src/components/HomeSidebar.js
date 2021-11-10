b083376cea3d316c6677aad80035d622ecc6ac0f
Keshav Kishor Ram <ramabishek40@gmail.com>
2021-11-10 11:27:35 +0545

    Feat: Make sidebar and set the sidebar and navbar as sticky

--- src/components/HomeSidebar.js
+++ src/components/HomeSidebar.js
@@ -1,9 +1,23 @@
 import React from "react";
 import "styles/homeSidebar.scss";
 import { Link } from "react-router-dom";
+import { FcBookmark, FcDocument, FcFile } from "react-icons/fc";
+import { IoNewspaperOutline, IoSettingsOutline } from "react-icons/io5";
+import { RiDraftLine } from "react-icons/ri";
+import { HiOutlineDocument } from "react-icons/hi";
+import { MdOutlineBookmarkBorder } from "react-icons/md";
+import { BsPeople, BsPersonCheck } from "react-icons/bs";
+import clsx from "clsx";
+import { useSelector } from "react-redux";
 export default function HomeSidebar() {
+  const { dark } = useSelector((state) => state.common);
   return (
-    <div className="home-sidebar">
+    <div
+      className={clsx(
+        "home-sidebar",
+        dark ? "home-sidebar-dark" : "home-sidebar-light"
+      )}
+    >
       <ul className="side-nav ps-2">
         <li className="side-nav-item">
           <Link to="/profile" className="nav-link p-0 profile-link">
@@ -15,19 +29,43 @@ export default function HomeSidebar() {
           </Link>
         </li>
         <li className="side-nav-item">
-          <span></span>My posts
+          <span>
+            <IoNewspaperOutline />{" "}
+          </span>
+          Posts
         </li>
         <li className="side-nav-item">
-          <span></span>Drafts
+          <span>
+            {" "}
+            <HiOutlineDocument />
+          </span>
+          Drafts
         </li>
         <li className="side-nav-item">
-          <span></span>Saved
+          <span>
+            <MdOutlineBookmarkBorder />
+          </span>
+          Saved
         </li>
         <li className="side-nav-item">
-          <span></span>Followers
+          <span>
+            <BsPeople />
+          </span>
+          Followers
         </li>
         <li className="side-nav-item">
-          <span></span>Following
+          <span>
+            <BsPersonCheck />
+          </span>
+          Following
+        </li>
+        <hr />
+
+        <li className="side-nav-item">
+          <span>
+            <IoSettingsOutline />
+          </span>
+          Settings
         </li>
       </ul>
     </div>
